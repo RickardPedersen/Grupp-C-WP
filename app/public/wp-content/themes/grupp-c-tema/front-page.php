1b21dac0c2fee652909429c8e69ee4d26d6b0358
lucas_inestal <lucasainestal@gmail.com>
2020-03-05 13:44:35 +0100

    styling front page, with utvalda posts only showing now

--- app/public/wp-content/themes/grupp-c-tema/front-page.php
+++ app/public/wp-content/themes/grupp-c-tema/front-page.php
@@ -44,35 +44,41 @@ while($homepageObjects->have_posts()) {
 			
 
 			<main class="site-main" id="main">
-            <?php $homepageObjects = new WP_Query(array("posts_per_page" => 4,
-"post_type" => "sales_object"));
+
+            <?php $homepageObjects = new WP_Query(array(
+                "posts_per_page" => 4,
+                "post_type" => "sales_object",
+                "meta_key" => "utvalda_objekt",
+                "meta_value" => "1"));
 
     while ($homepageObjects->have_posts()) {
     $homepageObjects->the_post();?>
-    <div class="object-summary">
-    <a class="objects-summary__date t-center" href="#">
-        <span class="objects-summary__day"><span>
+
+    
+    <div class="object-summary d-flex row">
+        <a class="objects-summary__date t-center" href="<?php the_permalink(); ?>">
+            <span class="objects-summary__day"><span>
         </a>
-        <div class="objects-summary__content shadow p-5 m-2 rounded">
-            <div class="objects-summary__thumbnail border border-dark rounded"><?php the_post_thumbnail(); ?></div><br>
-            <h5 class="event-summary__title headline headline--tiny"><a href="#">
+        <div class="objects-summary__content shadow m-2" style="background-image:url('<?php get_the_post_thumbnail_url($homepageObjects) ?>')">
+            <div class="objects-summary__thumbnail pl-2 pr-2 pt-2"><?php the_post_thumbnail(); ?></div><br>
+            <div class="row pl-4">
+                        <button class="ml-3 btn btn-dark  m-1"><?php echo get_post_meta($post->ID, 'antal_rum', true); ?></button>
+                        <button class="ml-1 btn btn-dark  m-1"><?php echo get_post_meta($post->ID, 'boarea', true); ?></button>
+                        <button class="ml-1 btn btn-dark  m-1">Utgångspris: <?php echo get_post_meta($post->ID, 'Utgångsbud', true); ?></button>
+                    </div>
+                <h5 class="event-summary__title headline headline--tiny pl-4 mt-4"><a href="<?php the_permalink(); ?>">
                 <?php the_title() ?></a></h5>
-                <div class="row">
-                <button class="ml-3 btn btn-dark m-1"><?php echo get_post_meta($post->ID, 'antal_rum', true); ?></button>
-                
-                </div>
-                <div class="row">
-                <button class="ml-3 btn btn-dark m-1"><?php echo get_post_meta($post->ID, 'boarea', true); ?></button>
-                <button class="ml-1 btn btn-dark m-1">Utgångspris: <?php echo get_post_meta($post->ID, 'Utgångsbud', true); ?></button>
-                </div><br>
-                <p><?php echo wp_trim_words(get_the_content(), 18);?> <a href="#" class="nu gray"
-        >Läs mer</a></p></div>
+                <p class=" pl-4 pb-4 pr-4"><?php echo wp_trim_words(get_the_content(), 35);?> <a class="text-primary" href="<?php the_permalink(); ?>" class="text-light"
+        >Läs mer</a></p>
         </div>
+    </div>
+    
         <?php
         }
         ?>
 			</main><!-- #main -->
-			<!-- The pagination component -->
+            <!-- The pagination component -->
+           
 			<?php understrap_pagination(); ?>
 
 			<!-- Do the right sidebar check -->
